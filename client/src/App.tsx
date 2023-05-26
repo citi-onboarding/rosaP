c0dd31a36ad972736ee9f942232a5dee2c50f4a2
Alice Peruniz <apo2@cin.ufpe.br>
2023-05-26 11:47:37 -0300

    feature(feature/navbar): navbar created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,12 @@ import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/global';
 
 
-import { Home } from './pages';
+import { NavBar } from './pages';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Home/>
+      <NavBar/>
       <GlobalStyle/>
     </ThemeProvider>
   );
--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,5 +1,17 @@
 import LogoCITi from './logo.png';
+import BackgroundImg from './image5.png'
+import Agulha from './agulha.png'
+import LogoRosa from './logo_rosa.png'
 
 export {
     LogoCITi
 };
+
+export {
+    BackgroundImg
+}
+
+export {
+    LogoRosa
+}
+
